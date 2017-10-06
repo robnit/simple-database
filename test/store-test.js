ddc5f845f06c9b78f88c085fc517ed98de39748e
Maryussuperboy <martsyalis@gmail.com>
2017-10-06 10:34:32 -0700

    writes and tests the hook

--- test/store-test.js
+++ test/store-test.js
@@ -5,13 +5,22 @@ const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 
 const store = require('../lib/make-store');
-
+const destination = './test-dir';
 
 describe('make-store.js', () => {
-    //TODO: before hook
+    beforeEach( done =>{
+        rimraf(destination, err =>{
+            if(err) return done(err);
+            mkdirp(destination, err =>{
+                if(err) return done(err);
+                done();
+            });
+        });
+    });
+    
     it('should save an object and get it based on ._id', () => {
         const myObject = { body : 'i like it :)' };
-        const destination = './test-dir';
+        
 
         const retrievedFile = store.get( writtenFile._id,  );
         const writtenFile = store.save( myObject, retrievedFile );
