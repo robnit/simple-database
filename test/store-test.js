4a9b0c8d9e204945d05d2372c04a3dc586645a25
Robin Miller <rmillerpdx@gmail.com>
2017-10-06 12:00:15 -0700

    begin save function

--- test/store-test.js
+++ test/store-test.js
@@ -1,33 +1,34 @@
 const assert = require('assert');
 // const fs = require('fs');
-// const path = require('path');
+const path = require('path');
 const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 
 const Store = require('../lib/make-store');
-const destination = './test-dir';
-let store = null;
+const rootDir = path.join(__dirname, 'test-dir');
+
+const store = new Store(rootDir);
+
 describe('make-store.js', () => {
 
     beforeEach( done =>{
-        rimraf(destination, err =>{
+        rimraf(rootDir, err =>{
             if(err) return done(err);
-            mkdirp(destination, err =>{
+            mkdirp(rootDir, err =>{
                 if(err) return done(err);
                 done();
             });
         });
-        store = new Store;
     });
 
     it('should save an object and get it based on ._id', (done) => {
-        const myObject = { body : 'i like it :)' };
+        const myObject = { data : 'i like it :)' };
         
-        store.save( myObject, (err, saved) => {
+        store.save(myObject, (err, saved) => {
             if(err) return done(err);
 
             assert.ok(saved._id);
-            assert.equal(saved.body, myObject.body);
+            assert.equal(saved.data, myObject.data);
 
             store.get(saved._id, (err, got) => {
                 if(err) return done(err);
