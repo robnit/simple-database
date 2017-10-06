56c6b4209c25b458b92ab6e2443c4db5cd4e00c7
Maryussuperboy <martsyalis@gmail.com>
2017-10-06 11:06:37 -0700

    rewrites test to be asyncronous

--- test/store-test.js
+++ test/store-test.js
@@ -6,8 +6,8 @@ const mkdirp = require('mkdirp');
 
 const store = require('../lib/make-store');
 const destination = './test-dir';
-
 describe('make-store.js', () => {
+
     beforeEach( done =>{
         rimraf(destination, err =>{
             if(err) return done(err);
@@ -17,15 +17,23 @@ describe('make-store.js', () => {
             });
         });
     });
-    
-    it('should save an object and get it based on ._id', () => {
+
+    it('should save an object and get it based on ._id', (done) => {
         const myObject = { body : 'i like it :)' };
         
+        store.save( myObject, (err,saved) => {
+            if(err) return done(err);
 
-        const retrievedFile = store.get( writtenFile._id,  );
-        const writtenFile = store.save( myObject, retrievedFile );
-        
-        assert.deepEqual( writtenFile, retrievedFile );
+            assert.ok(saved._id);
+            assert.equal(saved.body,myObject.body);
+
+            store.get(saved._id, (err, got) => {
+                if(err) return done(err);
+                
+                assert.deepEqual(saved,got);
+                done();
+            });
+        });
     });
 
 
