b0e85ea25854514950fcb45bbd543dcc05bbe965
Robin Miller <rmillerpdx@gmail.com>
2017-10-08 15:26:43 -0700

    discover bug with getAll test

--- test/store-test.js
+++ test/store-test.js
@@ -89,7 +89,14 @@ describe('make-store.js', () => {
                 });
             });
         });
+    });
 
+    it.skip('getAll() returns empty array if no files exist in directory', (done) => {
+        store.getAll( (err, objectArray) => {
+            if (err) return done(err);
+            assert.deepEqual( objectArray, [] );
+            done();
+        });
     });
 
 });
