4c2c0a1c0d615e218228aea8ad4cb01800ded8c3
Robin Miller <rmillerpdx@gmail.com>
2017-10-09 09:07:10 -0700

    remove unnecessary variable reassignment on .sort()

--- test/store-test.js
+++ test/store-test.js
@@ -88,11 +88,11 @@ describe('make-store.js', () => {
                 store.getAll( (err, objectArray) =>{
                     if (err) return done(err);
                     //Why does this need to be here in order for the code to work?
-                    const sortedExpected = expectedArray.sort(function(a, b){
+                    expectedArray.sort(function(a, b){
                         if(a._id < b._id) return -1;
                         if(a._id > b._id) return 1;
                     });
-                    assert.deepEqual( objectArray, sortedExpected);
+                    assert.deepEqual( objectArray, expectedArray);
                     done();
                 });
             });
