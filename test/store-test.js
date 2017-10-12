854565f8ffe28876896a16f67fd166150efc4059
Maryussuperboy <martsyalis@gmail.com>
2017-10-12 13:22:56 -0700

    everything but db-test

--- test/store-test.js
+++ test/store-test.js
@@ -1,109 +1,102 @@
 const assert = require('assert');
-
 const path = require('path');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
+const promisify = require('util').promisify;
+
+const rimraf = promisify(require('rimraf'));
+const mkdirp = promisify(require('mkdirp'));
 
 const Store = require('../lib/make-store');
 const rootDir = path.join(__dirname, 'test-dir');
 
 const store = new Store(rootDir);
 
-describe('make-store.js', () => {
+describe.only('make-store.js', () => {
 
-    beforeEach( done =>{
-        rimraf(rootDir, err =>{
-            if (err) return done(err);
-            mkdirp(rootDir, err =>{
-                if (err) return done(err);
-                done();
+    beforeEach( () =>{
+        return rimraf(rootDir)
+            .then ( () => {
+                return mkdirp(rootDir);
             });
-        });
     });
 
-    it('should save an object and get it based on ._id', (done) => {
+    it('should save an object and get it based on ._id', () => {
         const myObject = { data : 'i like it :)' };
         
-        store.save(myObject, (err, objectSaved) => {
-            if (err) return done(err);
-
-            assert.ok(objectSaved._id);
-            assert.equal(objectSaved.data, myObject.data);
-
-            store.get(objectSaved._id, (err, objectGot) => {
-                if (err) return done(err);
-
-                assert.deepEqual(objectSaved, objectGot);
-                done();
+        store.save(myObject)
+            .then ((data)=>{
+                assert.ok(data._id);
+                assert.equal(data.data, myObject.data);
+                store.get(data._id)
+                    .then((objectGot)=> {
+                        assert.deepEqual(data, objectGot);
+                    });
 
             });
 
-        });
-
     });
 
 
-    it('call callback with null if id is bad', (done) => {
-        store.get('bad id', (err, objectGot) => {
-            if (err) return done(err);
-            assert.deepEqual(objectGot, null);
-            done();
-        });
+    it('call callback with null if id is bad', () => {
+        store.get('bad id')
+            .then( (objectGot) =>{
+                assert.deepEqual(objectGot, null);
+            });
     });
 
 
-    it('remove should call the callback with {remove:true} if something was removed', (done) => {
+    it('remove should call the callback with {remove:true} if something was removed', () => {
         const myObject = { data : 'i like it removed please' };
-        store.save(myObject, (err, objectSaved) => {
-            if (err) return done(err);
-          
-            store.remove(objectSaved._id, (err, objectRemoved) => {
-                if (err) return done(err);
-                assert.deepEqual(objectRemoved, {removed: true});
-                done();
+        store.save(myObject)
+            .then ( (objectSaved) => {
+                store.remove(objectSaved._id)
+                    .then( (objectRemoved) => {
+                        assert.deepEqual(objectRemoved, {removed: true});
+                    });
             });
-        });
     });
 
 
-    it('remove should call the callback with {remove:false} if path did not exist', (done) => {
-        store.remove('rubbish', (err, objectRemoved) => {
-            if (err) return done(err);
-            assert.deepEqual(objectRemoved, {removed: false});
-            done();
-        });
+    it('remove should call the callback with {remove:false} if path did not exist', () => {
+        store.remove('rubbish')
+            .then( (objectRemoved) => {
+                assert.deepEqual(objectRemoved, {removed: false});
+            });
     });
 
-    it('get an array of objects from getAll method', (done) => {
+    it('get an array of objects from getAll method', () => {
         const objectOne = { data: 'cat' };
         const objectTwo = { data: 'dog' };
         const expectedArray = [];
         
-        store.save( objectOne, (err, savedObjectOne) => {
-            if (err) return done(err); 
-            expectedArray.push(savedObjectOne);
-            store.save( objectTwo, (err, savedObjectTwo) => {
-                if (err) return done(err);
-                expectedArray.push(savedObjectTwo);
-                store.getAll( (err, objectArray) => {
-                    if (err) return done(err);
-                    expectedArray.sort(function(a, b){
-                        if(a._id < b._id) return -1;
-                        if(a._id > b._id) return 1;
+        store.save( objectOne ) 
+            .then( (savedObjectOne) => {
+                expectedArray.push(savedObjectOne);
+            })
+
+            .then ( () =>{
+                store.save( objectTwo )
+                    .then( (savedObjectTwo) => {
+                        expectedArray.push(savedObjectTwo);
                     });
-                    assert.deepEqual( objectArray, expectedArray);
-                    done();
-                });
+            })
+
+            .then ( () => {
+                store.getAll()
+                    .then ( (objectArray)=> {
+                        expectedArray.sort(function(a, b){
+                            if(a._id < b._id) return -1;
+                            if(a._id > b._id) return 1;
+                        });
+                        assert.deepEqual( objectArray, expectedArray);
+                    });
             });
-        });
     });
 
-    it('getAll() returns empty array if no files exist in directory', (done) => {
-        store.getAll( (err, objectArray) => {
-            if (err) return done(err);
-            assert.deepEqual( objectArray, [] );
-            done();
-        });
+    it('getAll() returns empty array if no files exist in directory', () => {
+        store.getAll()
+            .then ( (objectArray) => {
+                assert.deepEqual( objectArray, [] );
+            });
     });
 
 });
