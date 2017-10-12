bcabc2c74ec1d3f90f839fc1d00341b736c38358
Maryussuperboy <martsyalis@gmail.com>
2017-10-12 11:03:25 -0700

    promisified db and test-db

--- test/db-test.js
+++ test/db-test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
+const promisify =require('util').promisify;
 const path = require('path');
-const rimraf = require('rimraf');
+const rimraf = promisify(require('rimraf'));
 const fs = require('fs');
+const readDir = promisify(fs.readdir);
+
 
 const Store = require('../lib/make-store');
 const Db = require('../lib/make-db');
@@ -11,61 +14,53 @@ const db = new Db(rootDir);
 
 describe('make-db.js', () => {
 
-    beforeEach( done => {
-        rimraf(rootDir, err => {
-            if (err) return done(err);
-            done();
-        });
+    beforeEach( () => rimraf(rootDir) );
+
+    it('calling getStore returns an instance of the Store class with correct rootDir property', () => {
+        db.getStore('dog')
+            .then ( (store) => assert.ok(store instanceof Store))
+            .then ( (store) =>assert.equal(store.rootDir, path.join(rootDir, 'dog')));
     });
 
-    it('calling getStore returns an instance of the Store class with correct rootDir property', (done) => {
-        db.getStore('dog', (err, store)=>{
-            if (err) return done(err);
-            assert.ok(store instanceof Store);
-            assert.equal(store.rootDir, path.join(rootDir, 'dog'));
-            done();
-        });
-    });
 
     it('create two files in a database and verify that they exist', (done) => {
-        db.getStore('dog', (err, store)=>{
-            const savedArray =[];
-            if (err) return done(err);
-            store.save({dog:'awsome'}, (err, savedObject1)=>{
-                if (err) return done(err);
-                savedArray.push(savedObject1._id+'.json');
-                store.save({cat: 'is evil'}, (err, savedObject2)=>{
+        db.getStore('dog')
+            .then ( (store)=> { 
+                const savedArray =[];
+                store.save({dog:'awsome'}, (err, savedObject1)=>{
                     if (err) return done(err);
-                    savedArray.push(savedObject2._id +'.json');
-                    const storePath = path.join(rootDir, 'dog');
-                    fs.readdir(storePath, 'utf8', (err, readFiles) => {
-                        readFiles.sort( (a, b) => {
-                            if(a._id < b._id) return -1;
-                            if(a._id > b._id) return 1;
+                    savedArray.push(savedObject1._id+'.json');
+                    store.save({cat: 'is evil'}, (err, savedObject2)=>{
+                        if (err) return done(err);
+                        savedArray.push(savedObject2._id +'.json');
+                        const storePath = path.join(rootDir, 'dog');
+                        fs.readdir(storePath, 'utf8', (err, readFiles) => {
+                            readFiles.sort( (a, b) => {
+                                if(a._id < b._id) return -1;
+                                if(a._id > b._id) return 1;
+                            });
+                            savedArray.sort( (a, b) => {
+                                if(a < b) return -1;
+                                if(a > b) return 1;
+                            });
+                            assert.deepEqual(readFiles, savedArray);
+                            done();
                         });
-                        savedArray.sort( (a, b) => {
-                            if(a < b) return -1;
-                            if(a > b) return 1;
-                        });
-                        assert.deepEqual(readFiles, savedArray);
-                        done();
                     });
                 });
             });
-        });
     });
 
-    it('create two store instances, verify that they exist', (done) => {
-        db.getStore('rat', (err) => {
-            if (err) return done(err);
-            db.getStore('bat', (err) => {
-                if (err) return done(err);
-                fs.readdir(rootDir, 'utf8', (err, names) => {
-                    assert.deepEqual(names, ['bat', 'rat']);
-                    done();
-                });
+    it('create two store instances, verify that they exist', () => {
+        db.getStore('rat')
+            .then( () => {
+                db.getStore('bat')
+                    .then (()=> {
+                        readDir(rootDir, 'utf8')
+                            .then((names) => {
+                                assert.deepEqual(names, ['bat', 'rat']); 
+                            });
+                    });
             });
-        });
     });
-
 });
