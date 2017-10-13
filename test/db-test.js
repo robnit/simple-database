a412a1d19cdec62a2364b8e246ee288e948981fa
Robin Miller <rmillerpdx@gmail.com>
2017-10-12 22:51:30 -0700

    fix return promise error, need to fix 2nd db test

--- test/db-test.js
+++ test/db-test.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
 const path = require('path');
 const promisify = require('util').promisify;
+const fs = require('fs');
 const rimraf = require('rimraf');
 const rimrafPromise = promisify(rimraf);
-const fs = require('fs');
+const readdirPromise = promisify(fs.readdir);
 
 const Store = require('../lib/make-store');
 const Db = require('../lib/make-db');
@@ -17,53 +18,48 @@ describe('make-db.js', () => {
         return rimrafPromise(rootDir);
     });
 
-    it.only('calling getStore returns an instance of the Store class with correct rootDir property', () => {
-        db.getStore('dog')
+    it('calling getStore returns an instance of the Store class with correct rootDir property', () => {
+        return db.getStore('dog')
             .then( store => {
                 assert.ok(store instanceof Store);
                 assert.equal(store.rootDir, path.join(rootDir, 'dog'));
             });
     });
 
-    it('create two files in a database and verify that they exist', (done) => {
-        db.getStore('dog', (err, store)=>{
-            const savedArray =[];
-            if (err) return done(err);
-            store.save({dog:'awsome'}, (err, savedObject1)=>{
-                if (err) return done(err);
-                savedArray.push(savedObject1._id+'.json');
-                store.save({cat: 'is evil'}, (err, savedObject2)=>{
-                    if (err) return done(err);
-                    savedArray.push(savedObject2._id +'.json');
-                    const storePath = path.join(rootDir, 'dog');
-                    fs.readdir(storePath, 'utf8', (err, readFiles) => {
-                        readFiles.sort( (a, b) => {
-                            if(a._id < b._id) return -1;
-                            if(a._id > b._id) return 1;
-                        });
-                        savedArray.sort( (a, b) => {
-                            if(a < b) return -1;
-                            if(a > b) return 1;
-                        });
-                        assert.deepEqual(readFiles, savedArray);
-                        done();
-                    });
-                });
+    it('create two files in a database and verify that they exist', () => {
+        let savedArray = null;
+        return db.getStore('dog')
+            .then( (store) => {
+                return Promise.all([
+                    store.save({dog:'awsome'}),
+                    store.save({cat: 'is evil'})
+                ]);
+            })
+            .then( (saved) => {
+                savedArray = saved.sort((a,b) => a._id > b._id ? -1 : 1);
+                console.log('saved array is', savedArray);
+                const storePath = path.join(rootDir, 'dog');
+                return readdirPromise(storePath);
+            })
+            .then( (readFiles) => {
+                console.log('readFiles is', readFiles);
+                readFiles.sort((a,b) => a._id > b._id ? -1 : 1);
+                return assert.deepEqual(readFiles, savedArray);
             });
-        });
     });
 
-    it('create two store instances, verify that they exist', (done) => {
-        db.getStore('rat', (err) => {
-            if (err) return done(err);
-            db.getStore('bat', (err) => {
-                if (err) return done(err);
-                fs.readdir(rootDir, 'utf8', (err, names) => {
-                    assert.deepEqual(names, ['bat', 'rat']);
-                    done();
-                });
-            });
-        });
-    });
+
+    // it('create two store instances, verify that they exist', (done) => {
+    //     db.getStore('rat', (err) => {
+    //         if (err) return done(err);
+    //         db.getStore('bat', (err) => {
+    //             if (err) return done(err);
+    //             fs.readdir(rootDir, 'utf8', (err, names) => {
+    //                 assert.deepEqual(names, ['bat', 'rat']);
+    //                 done();
+    //             });
+    //         });
+    //     });
+    // });
 
 });
